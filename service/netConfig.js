abc654c958f75ae839c1263aa195400e1266b6fa
Alex-rightMa <alexma2013@163.com>
2020-07-13 13:43:35 +0800

    增加npm命令，同时启动vue和koa项目

--- service/netConfig.js
+++ service/netConfig.js
@@ -1,6 +1,9 @@
+let user = process.argv[2] ;
+let password = process.argv[3];
+
 module.exports = {
     port: 3000,
     password: 'manguodbgreat',
-    mongooseStr: 'mongodb+srv://admin:manguodbgreat@publicrog-kn7zd.mongodb.net/test?retryWrites=true&w=majority',
+    mongooseStr: `mongodb+srv://${user}:${password}@publicrog-kn7zd.mongodb.net/test?retryWrites=true&w=majority`,
     secret: 'zhihu-jwt-20200601',
 }
